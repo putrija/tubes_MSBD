b1ac7af635d4b6d230866ef8861ad03c1d315d4e
ganteng <erastusganteng1@gmail.com>
2022-11-21 17:01:32 +0700

    Erastus - Tambah halaman siswa

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/siswa', function () {
+    return view('halaman_siswa.siswa');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
