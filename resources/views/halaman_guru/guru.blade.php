36da9611b3fb4d94f2bca77d03a370f8a1e275b3
putrija <91309853+putrija@users.noreply.github.com>
2022-11-26 10:16:41 +0700

    menambahkan css dan asset -putrija

---
+++ resources/views/halaman_guru/guru.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Profil Guru</title>
+</head>
+<body>
+  <div class="row">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+
+        Detail Siswa          </div>
+      <div class="panel-body">
+        
+<style media="screen">
+.col-md-6{
+padding-bottom: 30px;
+}
+</style>
+<div class="col-md-6">
+<table class="table">
+<tbody><tr>
+  <td>Nama Lengkap</td>
+  <td>:</td>
+  <td>A Imam Taufik</td>
+</tr>
+<tr>
+  <td>NISN</td>
+  <td>:</td>
+  <td>9985856331</td>
+</tr>
+<tr>
+  <td>NIS</td>
+  <td>:</td>
+  <td>141510121</td>
+</tr>
+<tr>
+  <td>Kelas</td>
+  <td>:</td>
+  <td>12</td>
+</tr>
+<tr>
+  <td>Rombel</td>
+  <td>:</td>
+  <td>12 AK1</td>
+</tr>
+<tr>
+  <td>Jenis Kelamin</td>
+  <td>:</td>
+  <td>L</td>
+</tr>
+</tbody></table>
+<br>
+<a href="http://localhost/siakad/admin/edit-siswa/1" class="btn btn-primary">Edit</a>
+<a href="http://localhost/siakad/admin/siswa" class="btn btn-default">Kembali</a>
+</div>
+<div class="col-md-6">
+
+  <img src="http://localhost/siakad/images/siswa/male.jpg" width="300px" class="img img-responsive" alt="">
+</div>
+      </div> <!-- end of class panel-body -->
+    </div> <!-- end of class panel -->
+  </div>
+</body>
+</html>
