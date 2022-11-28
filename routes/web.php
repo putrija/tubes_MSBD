9eb3f577dcaa26192c52fe4164c4a1d85553b9e0
putrija <91309853+putrija@users.noreply.github.com>
2022-11-28 09:40:14 +0700

    menambahkan frontend lihat nilai dan edit nilai -putrija

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::get('/', function () {
 
 //Guru
 Route::get('/guru', [App\Http\Controllers\TeacherController::class, 'index'])->name('guru');
-Route::get('/guru/edit', [App\Http\Controllers\TeacherController::class, 'edit'])->name('editguru');
+Route::get('/guru/edit', [App\Http\Controllers\TeacherController::class, 'edit'])->name('edit_guru');
+
+//Nilai
+Route::get('/nilai', [App\Http\Controllers\StudentsReportController::class, 'index'])->name('nilai');
+Route::get('/nilai/edit', [App\Http\Controllers\StudentsReportController::class, 'edit'])->name('edit_nilai');
 
 Route::get('/siswa', function () {
     return view('halaman_siswa.siswa', [
