6cf8ef620067e302103d6095e191fc519bd5478e
PutriAnisa123 <pa374167@gmail.com>
2022-11-29 13:59:39 +0700

    putrii anisa
    Signed-off-by: PutriAnisa123 <pa374167@gmail.com>

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -41,6 +41,8 @@
         </div>
       </nav>
 
+      @include('layouts.sidebar_admin')
+
       @yield('konten')
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
