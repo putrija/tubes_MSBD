53f484143b6423e9c394e174f99dbb215f51acfb
putrija <91309853+putrija@users.noreply.github.com>
2022-11-30 08:44:15 +0700

    mengubah migration teacher -putrija

--- database/migrations/2022_11_21_034122_create_teachers_table.php
+++ database/migrations/2022_11_21_034122_create_teachers_table.php
@@ -14,17 +14,28 @@ return new class extends Migration
     public function up()
     {
         Schema::create('teachers', function (Blueprint $table) {
-            $table->string('nip', 20)->primary();
+            $table->integer('id_guru', 11)->primary();
+            $table->char('nip', 18)->unique();
+            $table->char('nip', 16)->unique();
             $table->string('nama', 255);
-            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
-            $table->string('tempat_lahir');
+            $table->string('tempat_lahir', 100);
             $table->dateTime('tanggal_lahir');
-            $table->enum('status_kepegawaian', ['PNS', 'PPPK', 'GTY/PTY', 'Guru Honor Sekolah']);
-            $table->string('jenis_ptk');
-            $table->enum('agama', ['Islam', 'Kristen', 'Katolik', 'Budhha', 'Hindu', 'Konghucu', 'Aliran Kepercayaan']);
-            $table->string('no_tlp', 15);
-            $table->string('email')->unique();
-            $table->enum('status_pernikahan', ['Menikah', 'Belum Menikah']);
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
+            $table->enum('jenis_ptk', ['Guru TIK', 'Guru Mapel', 'Guru BK', 'Guru Kelas']);
+            $table->enum('agama', ['Islam', 'Kristen', 'Katolik', 'Buddha', 'Hindu', 'Konghucu', 'Aliran Kepercayaan']);
+            $table->string('nama_ibu_kandung', 255);
+            $table->string('no_tlp', 20);
+            $table->string('no_hp', 20);
+            $table->string('email', 255)->unique();
+            $table->text('tugas_tambahan');
+            $table->enum('sudah_lesensi_kepsek', ['Ya', 'Tidak']);
+            $table->enum('pernah_diklat_pengawasan', ['Ya', 'Tidak']);
+            $table->enum('keahlian_braile', ['Ya', 'Tidak']);
+            $table->enum('keahlian_bahasa_isyarat', ['Ya', 'Tidak']);
+            $table->string('kewarganegaraan', 255);
+            $table->char('nik', 16);
+            $table->char('no_kk', 16);
+            $table->string('nuks', 255);
         });
     }
 
