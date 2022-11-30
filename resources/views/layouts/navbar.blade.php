e1e086b223f16e604495f6fbc9b02ac0445dc919
putrija <91309853+putrija@users.noreply.github.com>
2022-11-30 08:02:26 +0700

    .

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,14 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<<<<<<< HEAD
     {{-- <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet"/>
     <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet"/> --}}
     @yield('link_css')
-=======
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet"/>
->>>>>>> f147ca4809a881405985d3f24f17b2e4affead5b
 
     {{-- <title>{{ $titlenya }}</title> --}}
     <link rel="icon" type="image/x-icon" href="/images/logo_2.png">
